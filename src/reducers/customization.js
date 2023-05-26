031d149757430e3bfd28528462ebc6698db9cd87
ShahzodZiyodullayev <sshahzod5@gmail.com>
2023-05-27 04:47:51 +0500

    mode kunduzgi rejimiga almashtirildi

--- src/reducers/customization.js
+++ src/reducers/customization.js
@@ -1,50 +1,48 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const whichMode = localStorage.getItem("mode")
-  ? JSON.parse(localStorage.getItem("mode"))
-  : true;
+const whichMode = localStorage.getItem("mode") ? JSON.parse(localStorage.getItem("mode")) : true;
 
 const otherStyles = {
-  // sidesPadding: 3,
+    // sidesPadding: 3,
 };
 
 const initialState = (bool = whichMode) => {
-  localStorage.setItem("mode", bool);
-  switch (bool) {
-    case true:
-      return {
-        mode: "light",
-        bool,
-        styles: {
-          primaryTextColor: "#010A19",
-          background: "#fff",
-        },
-      };
-    case false:
-      return {
-        mode: "dark",
-        bool,
-        styles: {
-          primaryTextColor: "#fff",
-          background: "#010A19",
-        },
-      };
-  }
+    localStorage.setItem("mode", bool);
+    switch (bool) {
+        case false:
+            return {
+                mode: "light",
+                bool,
+                styles: {
+                    primaryTextColor: "#010A19",
+                    background: "#fff"
+                }
+            };
+        case true:
+            return {
+                mode: "dark",
+                bool,
+                styles: {
+                    primaryTextColor: "#fff",
+                    background: "#010A19"
+                }
+            };
+    }
 };
 
 export const customizationReducer = createSlice({
-  name: "customization",
-  initialState: { ...initialState(), ...otherStyles },
-  reducers: {
-    themeMode: (state, { _, payload }) => {
-      state = { ...initialState(payload), ...otherStyles };
-      return state;
-    },
-    otherStylesMode: (state, { _, payload }) => {
-      state = { ...state, ...payload };
-      return state;
-    },
-  },
+    name: "customization",
+    initialState: { ...initialState(), ...otherStyles },
+    reducers: {
+        themeMode: (state, { _, payload }) => {
+            state = { ...initialState(payload), ...otherStyles };
+            return state;
+        },
+        otherStylesMode: (state, { _, payload }) => {
+            state = { ...state, ...payload };
+            return state;
+        }
+    }
 });
 
 export const { themeMode, otherStylesMode } = customizationReducer.actions;
